08a9960ff30f370542980fb6448a7cca31aaac5a
oveldman <oveldman@gmail.com>
2025-05-11 15:35:20 +0200

    Connect grpc in the client

--- src/MadWorldNL.StrixAluco.Clients.ControlPanel.Web/Program.cs
+++ src/MadWorldNL.StrixAluco.Clients.ControlPanel.Web/Program.cs
@@ -1,3 +1,4 @@
+using Grpc.Net.Client.Web;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MadWorldNL.StrixAluco;
@@ -7,5 +8,13 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services
+    .AddGrpcClient<Greeter.GreeterClient>(options =>
+    {
+        options.Address = new Uri("https://localhost:7286");
+    })
+    .ConfigurePrimaryHttpMessageHandler(
+        () => new GrpcWebHandler(new HttpClientHandler()));
 
 await builder.Build().RunAsync();
--- src/MadWorldNL.StrixAluco.Server.Controllers.Web/Program.cs
+++ src/MadWorldNL.StrixAluco.Server.Controllers.Web/Program.cs
@@ -6,10 +6,23 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddGrpc();
 builder.Services.AddGrpcReflection();
 
+builder.Services.AddCors(o => o.AddPolicy("AllowAll", builder =>
+{
+    builder.AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader()
+        .WithExposedHeaders("Grpc-Status", "Grpc-Message", "Grpc-Encoding", "Grpc-Accept-Encoding");
+}));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
+app.UseGrpcWeb(new GrpcWebOptions { DefaultEnabled = true });
+app.UseCors();
+
+app.MapGrpcService<GreeterService>()
+    .RequireCors("AllowAll");
+
 app.MapGet("/",
     () =>
         "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
