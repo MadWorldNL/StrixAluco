4f29a7adb343cdb9c08857d0a6a4dc31bc47d417
oveldman <oveldman@gmail.com>
2025-05-11 18:49:33 +0200

    Add appsettings.json to client

--- src/MadWorldNL.StrixAluco.Clients.ControlPanel.Web/Program.cs
+++ src/MadWorldNL.StrixAluco.Clients.ControlPanel.Web/Program.cs
@@ -7,12 +7,15 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Configuration.AddJsonFile("appsettings.json");
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services
     .AddGrpcClient<Greeter.GreeterClient>(options =>
     {
-        options.Address = new Uri("https://localhost:7286");
+        var serverUrl = builder.Configuration.GetValue<string>("Server:Url");
+        options.Address = new Uri(serverUrl!);
     })
     .ConfigurePrimaryHttpMessageHandler(
         () => new GrpcWebHandler(new HttpClientHandler()));
